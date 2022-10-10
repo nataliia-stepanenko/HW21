9d9bc59e2548d935d047751192dd44ee5a356e6c
nataliia-stepanenko <natalyss.contact@gmail.com>
2022-10-11 01:28:10 +0300

    Turned 'type' into a style attribute, font-weight property

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,5 @@
-const Title = ({ name, type }) => {
-    return <h2 className={type}>{name}</h2>;
+const Title = ({ name, type:fontWeight}) => {
+    return <h2 style={{ fontWeight }}>{name}</h2>;
 };
 
 export default Title;
